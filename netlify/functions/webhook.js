628fb4d22f31937ae12af9d99e5c0804eae04df6
JuanBifrost <163063048+JuanBifrost@users.noreply.github.com>
2024-08-26 22:47:30 -0500

    Update webhook.js

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -1,39 +1,18 @@
-const axios = require('axios');
-
-exports.handler = async (event) => {
-    const requestData = {
-        MessageID: 1,
-        Password: '2yQQC[<6KiXuMM];%R&lv>-1,)a/Cfheacbc~|wI+DE:19[w4$TAqb9GX(~r^.',
-        MessageType: 'BopJob',
-        PhoneNumber: event.PhoneNumber,
-        ApiVersion: 5,
-        BOP: true,
-        CLI: false
+// netlify/functions/webhook.js
+exports.handler = async (event, context) => {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: 'Method Not Allowed',
     };
+  }
 
-    const ivrServiceUrl = 'https://66c4e98ac73f903d5593bc67--bifrost-sofia.netlify.app/ivr-endpoint';
+  const data = JSON.parse(event.body);
 
-    try {
-        const response = await axios.post(ivrServiceUrl, requestData);
-        console.log('Respuesta del servidor:', response.data);
+  console.log('Webhook data received:', data);
 
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'Request enviado exitosamente',
-                data: response.data
-            }),
-        };
-    } catch (error) {
-        console.error('Error al enviar el request:', error);
-
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: 'Error al enviar el request',
-                error: error.message
-            }),
-        };
-    }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Webhook received successfully' }),
+  };
 };
-
